7f131bbe81eb058dc8d1edb10de3b0b4a0945749
akashvelmurugan <akashvelmurugan125@gmail.com>
2022-02-22 22:37:00 +0530

    PEACEFULLPARTY

---
+++ src/PeacefullOffParty.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PeacefullOffParty {
+	public static void main(String[] args) {
+
+		Scanner scanner= new Scanner(System.in);
+
+		int size = scanner.nextInt();
+		for (int i = 0; i < size; i++) {
+			int array[]=new int[size];
+			for (int j = 0; j <size; j++) {
+				array[j]= scanner.nextInt();
+			}
+			if (array[0]+array[2]>array[1]) {
+				System.out.println(array[0]+array[2]);
+			} else {
+				System.out.println(array[1]);
+			}
+		}
+
+	}
+}
